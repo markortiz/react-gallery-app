c50d27a3f27d65bcdcb703bf7b4cb848195878ac
un32dp <mark.kenneth.ortiz@asia.ing.com>
2019-12-08 23:10:37 +0800

    add gallery and lazy image

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Gallery.css';
+import { LazyLoadImage, trackWindowScroll } from 'react-lazy-load-image-component';
+
+
+
+function Gallery({ images, scrollPosition }) {
+  return (
+    <div className="Gallery row">
+      {
+        images && images.map(({ alt_description, urls }, index) => (
+          <div key={index} className="Gallery-card col-md-4">
+            <LazyLoadImage
+              className="Gallery-image mb-4 shadow-lg"
+              alt={alt_description}
+              effect="blur"
+              scrollPosition={scrollPosition}
+              src={urls.small}/>
+          </div>
+        ))
+      }
+    </div>
+  );
+}
+
+export default trackWindowScroll(Gallery);

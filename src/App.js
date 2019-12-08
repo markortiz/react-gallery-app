57a88f955a720e8d9e2d896e170a077f5afcb77d
un32dp <mark.kenneth.ortiz@asia.ing.com>
2019-12-08 16:56:42 +0800

    add thumbnail component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Thumbnail from './components/Thumbnail/Thumbnail';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <div className="row">
+            <div className="col-8">
+              <h4>Album</h4>
+            </div>
+          </div>
+        </div>
       </header>
+      <main role="main">
+        <section className="App-highlight jumbotron text-center">
+          <div className="container">
+            <h1>Album example</h1>
+            <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" placeholder="Keyword" aria-label="Search" aria-describedby="button-addon2" />
+              <div className="input-group-append">
+                <button className="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="Album">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4"><Thumbnail /></div>
+              <div className="col-md-4"><Thumbnail /></div>
+              <div className="col-md-4"><Thumbnail /></div>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }

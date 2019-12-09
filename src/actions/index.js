e9857bba8598cda8b53a033683f307e7476e4bca
un32dp <mark.kenneth.ortiz@asia.ing.com>
2019-12-09 23:50:25 +0800

    update actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,3 @@
 export {
-  fetchPhotos,
+  searchPhotos
 } from './photos';
--- src/actions/photos.js
+++ src/actions/photos.js
@@ -1,32 +1,59 @@
-const url = "https://api.unsplash.com";
+import { setPhotoList } from '../reducer';
+
+const baseUrl = "https://api.unsplash.com";
 const clientID = 'Client-ID 8f9fbd10d8bb0a7e69dd531aea77d5a0b84152b806286ed7f83f896c1987413b';
 const headers = { 
   'Authorization': clientID,
   'Accept-Version': 'v1',
   'Content-Type': 'application/json',
 };
-const fetchPhotos = (store, page = 1) => {
-  fetch(`${url}/photos?page=${page}`, { headers }).then((response) => response.json())
-    .then((response) => {
-      // Let's check if response is an Array.
-      if(response && response.constructor === Array) {
-        const { photos } = store.state;
 
-        store.setState({
-          photos: [ ...photos, ...response ],
-          photosPage: page,
-        });
-      } else {
-        store.setState({
-          error: "Something went wrong..."
-        });
+const constructUrlParams = (url, params) => {
+  let strParams = '';
+  // Convert params object to array to loop.
+  Object.entries(params)
+        .forEach(([key, value]) => {
+          // If has a value add to strParams.
+          if(value) {
+             strParams += `&${key}=${value}`;
+          }
+        })
+  const urlWithParams = url + `?${strParams.substr(1)}`;
+
+  return urlWithParams;
+}
+
+const searchPhotos = (store, params = {}) => {
+  const { isSearch, page, perPage, query, orientation } = params;
+  const currentPage = page || 1;
+  const urlParams = {
+    orientation: orientation || '',
+    page: currentPage,
+    per_page: perPage || 10,
+    query: query || '',
+  };
+  const url = isSearch ? constructUrlParams(`${baseUrl}/search/photos`, urlParams)
+                       : constructUrlParams(`${baseUrl}/photos`, urlParams);
+
+  fetch(url, { headers }).then((response) => response.json())
+    .then((response) => {
+      const payload = {
+        isSearch,
+        currentPage,
+        perPage,
+        orientation,
+        query,
+        response,
       }
+
+      setPhotoList(store, payload);
     })
     .catch((error) => {
       alert('Something went wrong...');
+      console.error(error);
     });
 }
 
 export {
-  fetchPhotos,
+  searchPhotos,
 }

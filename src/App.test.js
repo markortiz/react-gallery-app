5511cc6de7bdc639020b8c630b90c3191eaef183
un32dp <mark.kenneth.ortiz@asia.ing.com>
2019-12-12 01:34:35 +0800

    setup unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,57 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App.js', () => {
+  let headers, mockSuccessResponse;
+
+  beforeEach(() => {
+    headers = { 
+      'Authorization': '',
+      'Accept-Version': 'v1',
+      'Content-Type': 'application/json',
+    };
+    mockSuccessResponse = {
+      "result": true,
+      "response": [{
+          "id": "e4ae2a63-44d3-4bbb-8168-c6741932a08a",
+          "name": "Work",
+          "fullName": "Berlin, Germany",
+          "location": {
+              "lat": 52.52000659999999,
+              "lng": 13.404954
+          }
+      }]
+    };
+  });
+
+  // afterEach(() => {
+  //   global.fetch.mockClear();
+  // });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('fetches photos from server on load', (done) => {
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise,
+    });
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+                        
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith('https://api.unsplash.com/photos?page=1&per_page=10', { headers });
+
+    process.nextTick(() => {
+      global.fetch.mockClear();
+      done();
+    });
+  });
 });
